94f56e5ad1ba39e3e5d22aba3dd37a687d0587c1
tilaknihith <venkatkorra84@gmail.com>
2023-08-01 10:01:21 +0530

    add the address table for the app

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_29_123640) do
+ActiveRecord::Schema.define(version: 2023_08_01_042922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "location"
+    t.integer "pincode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
